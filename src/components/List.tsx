5d5e9ba9f60a6d0d34b7a7f2bc1a1b05632d3bd9
eg-bond <eg-bond@yandex.ru>
2024-10-02 13:58:11 +0300

    feat: List component upgrade
    1. 2 more unit tests for List component done
    2. List component updated to match test cases

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,19 +1,38 @@
-import { Item } from "./Item";
+import { Item } from './Item';
 
 type Props = {
   items: Task[];
-  onDelete: (id: Task["id"]) => void;
-  onToggle: (id: Task["id"]) => void;
+  onDelete: (id: Task['id']) => void;
+  onToggle: (id: Task['id']) => void;
 };
 
-export const List = ({ items, onDelete, onToggle }: Props) => (
-  <ul className="task-list tasks">
-    {items.map((item) => (
-      <Item
-        {...item}
-        key={item.id}
-        onDelete={onDelete}
-        onToggle={onToggle} />
-    ))}
-  </ul>
-);
+const MAX_ITEMS_TO_SHOW = 10;
+
+export const List = ({ items, onDelete, onToggle }: Props) => {
+  const activeTasks = items.filter(item => !item.done);
+  if (activeTasks.length > MAX_ITEMS_TO_SHOW) {
+    items = items.slice(0, 10);
+  }
+
+  return (
+    <>
+      <ul className='task-list tasks'>
+        {items.map(item => (
+          <Item
+            {...item}
+            key={item.id}
+            onDelete={onDelete}
+            onToggle={onToggle}
+          />
+        ))}
+      </ul>
+      {activeTasks.length > MAX_ITEMS_TO_SHOW && (
+        <div className='empty-wrapper'>
+          В списке есть еще {activeTasks.length - 10} активных задачи, но вы не
+          можете их видеть, так как их слишком много. <br />
+          Показаны первые 10 задач.
+        </div>
+      )}
+    </>
+  );
+};
