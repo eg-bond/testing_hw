deca295e6b9ab726846d24911eaa237b603064dc
eg-bond <eg-bond@yandex.ru>
2024-10-03 11:51:09 +0300

    test: first integration test for Notifier component done

--- spec/integration/Notifier.spec.tsx
+++ spec/integration/Notifier.spec.tsx
@@ -1,7 +1,53 @@
-import { render, screen } from "@testing-library/react";
-import { Notifier } from "src/components/Notifier";
+import { render, screen } from '@testing-library/react';
+import { Notifier } from 'src/components/Notifier';
+import { TaskList } from 'src/modules/TaskList';
+import { JestStoreProvider } from '../utils/JestStoreProvider';
+import ue from '@testing-library/user-event';
+import { NotifierContainer } from 'src/modules/NotifierContainer';
+import { store } from 'src/store/configureStore';
+import { addTask } from 'src/store/taskSlice';
 
-describe('Оповещение при вополнении задачи', () => {
-    it.todo('появляется и содержит заголовок задачи');
-    it.todo('одновременно может отображаться только одно');
+const items = [
+  {
+    header: 'купить хлеб',
+    done: false,
+  },
+  {
+    header: 'купить молоко',
+    done: false,
+  },
+  {
+    header: 'выгулять собаку',
+    done: false,
+  },
+];
+
+const userEvent = ue.setup({
+  advanceTimers: jest.advanceTimersByTime,
+});
+
+describe('Оповещение при выполнении задачи', () => {
+  it('появляется и содержит заголовок задачи', async () => {
+    // setup test state
+    store.dispatch(addTask(items[0].header));
+
+    render(
+      <>
+        <TaskList />
+        <NotifierContainer />
+      </>,
+      {
+        wrapper: JestStoreProvider,
+      }
+    );
+
+    const taskCheckbox = screen.getByRole('checkbox');
+    await userEvent.click(taskCheckbox);
+
+    const notifierWrapper = screen.getByTestId('notifier-wrapper');
+
+    expect(notifierWrapper).toBeInTheDocument();
+    expect(notifierWrapper).toHaveTextContent(items[0].header);
+  });
+  it.todo('одновременно может отображаться только одно');
 });
